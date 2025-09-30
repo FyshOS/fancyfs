7a6858c5bd2562456a03d01271a0c24d1a68e032
Andy Williams <andy@andy.xyz>
2025-09-30 15:18:01 +0100

    Update to have icons for known folders
    And users can set that with .background.svg too

--- folder.go
+++ folder.go
@@ -3,10 +3,12 @@ package fancyfs
 import (
 	"errors"
 	"os"
+	"path/filepath"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/storage"
+	"fyne.io/fyne/v2/theme"
 )
 
 var ErrNoMetadata = errors.New("no metadata for requested folder")
@@ -18,25 +20,65 @@ type FancyFolder struct {
 }
 
 func DetailsForFolder(dir fyne.URI) (*FancyFolder, error) {
-	err := ErrNoMetadata
+	home, err := os.UserHomeDir()
+	if err == nil {
+		if dir.Path() == home {
+			return &FancyFolder{
+				BackgroundResource: theme.HomeIcon(),
+				BackgroundFill:     canvas.ImageFillContain,
+			}, nil
+		}
 
+		if isSubDir(dir.Path(), home) {
+			var res fyne.Resource
+			switch dir.Name() {
+			case "Desktop":
+				res = theme.DesktopIcon()
+			case "Documents":
+				res = theme.DocumentIcon()
+			case "Downloads":
+				res = theme.DownloadIcon()
+			case "Music":
+				res = theme.MediaMusicIcon()
+			case "Pictures":
+				res = theme.MediaPhotoIcon()
+			case "Videos", "Movies":
+				res = theme.MediaVideoIcon()
+			}
+
+			if res != nil {
+				return &FancyFolder{
+					BackgroundResource: res,
+					BackgroundFill:     canvas.ImageFillContain,
+				}, nil
+			}
+		}
+	}
+
+	err = ErrNoMetadata
 	bg, err1 := checkBGImage(dir, ".background.png")
-	if err1 == nil {
+	if bg != nil {
 		return bg, nil
 	} else if err1 != ErrNoMetadata {
 		err = err1
 	}
 	bg, err2 := checkBGImage(dir, ".background.jpg")
-	if err2 == nil {
+	if bg != nil {
 		return bg, nil
 	} else if err2 != ErrNoMetadata {
 		err = err2
 	}
 	bg, err3 := checkBGImage(dir, ".background.jpeg")
-	if err3 == nil {
+	if bg != nil {
 		return bg, nil
 	} else if err3 != ErrNoMetadata {
 		err = err3
+	}
+	bg, err4 := checkBGImage(dir, ".background.svg")
+	if bg != nil {
+		return bg, nil
+	} else if err4 != ErrNoMetadata {
+		err = err4
 	}
 
 	return nil, err
@@ -53,8 +95,24 @@ func checkBGImage(dir fyne.URI, name string) (*FancyFolder, error) {
 		return nil, err
 	}
 
+	if filepath.Ext(name) == ".svg" {
+		res, err := fyne.LoadResourceFromPath(bgFile.Path())
+		if err == nil {
+			return &FancyFolder{
+				BackgroundResource: theme.NewThemedResource(res),
+				BackgroundFill:     canvas.ImageFillContain,
+			}, nil
+		}
+	}
+
 	return &FancyFolder{
 		BackgroundURI:  bgFile,
 		BackgroundFill: canvas.ImageFillCover,
 	}, nil
+}
+
+func isSubDir(dir, home string) bool {
+	parent := filepath.Dir(dir)
+
+	return parent == home
 }
